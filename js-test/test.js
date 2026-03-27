d99e263990489421a739f895072754d85a31127f
DESKTOP-S606IMK\user <j4087j@naver.com>
2026-03-27 15:57:04 +0900

    test 만듦

---
+++ js-test/test.js
@@ -0,0 +1,10 @@
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
